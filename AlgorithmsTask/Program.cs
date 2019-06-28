da8c5ee7a3c99f2611ae2189293bc1500d85c12f
NATHAN TONG <102090809@student.swin.edu.au>
2019-06-28 14:01:46 +1000

    First Commit, Inserting File Path

---
+++ AlgorithmsTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
+
+namespace AlgorithmsTask
+{
+    class Program
+    {
+        static List<int> ShellList = new List<int>();
+        static List<int> InsertionList = new List<int>();
+        static List<int> Items = new List<int>();
+        static List<int> End = new List<int>();
+
+        public static string filePath = @"C:\Users\Nathan\source\repos\AlgorithmsTask\Files\unsorted_numbers";
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
